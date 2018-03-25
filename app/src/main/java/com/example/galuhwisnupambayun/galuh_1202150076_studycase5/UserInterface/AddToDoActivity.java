41534bb33e0a7c996e7447e335e1adf37d77719f
galuhwis <atp.sports97@gmail.com>
2018-03-25 22:12:10 +0700

    bismillah

---
+++ app/src/main/java/com/example/galuhwisnupambayun/galuh_1202150076_studycase5/UserInterface/AddToDoActivity.java
@@ -0,0 +1,46 @@
+package com.example.galuhwisnupambayun.galuh_1202150076_studycase5.UserInterface;
+
+/**
+ * Created by Galuh Wisnu Pambayun on 25/03/2018.
+ */
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.galuhwisnupambayun.galuh_1202150076_studycase5.R;
+
+public class AddToDoActivity extends AppCompatActivity {
+    private EditText Eactivity, Edescription, Epriority;
+    private Button Simpan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_to_do);
+
+        Eactivity = findViewById(R.id.Etodo);
+        Edescription = findViewById(R.id.Edesc);
+        Epriority = findViewById(R.id.Eprio);
+        Simpan = findViewById(R.id.save);
+
+        Simpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("activity",Eactivity.getText().toString());
+                intent.putExtra("desc",Edescription.getText().toString());
+                intent.putExtra("priority",Epriority.getText().toString());
+                setResult(1,intent);
+                finish();//finishing activity
+            }
+        });
+    }
+}
